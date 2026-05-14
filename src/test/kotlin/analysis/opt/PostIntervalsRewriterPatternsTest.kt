5c76eb1230fd61bc3dd35e9b0ce83b43e1608227
yoav-rodeh <80630459+yoav-rodeh@users.noreply.github.com>
2026-05-14 13:54:13 +0300

    replace div comparison patterns with multiplication expressions. (#8518)
    e8229d01354cc2aa0cf344179914f9ff8164e25f

---
+++ src/test/kotlin/analysis/opt/PostIntervalsRewriterPatternsTest.kt
@@ -0,0 +1,111 @@
+/*
+ *     The Certora Prover
+ *     Copyright (C) 2025  Certora Ltd.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package analysis.opt
+
+import analysis.opt.PatternRewriter.PatternHandler
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import vc.data.TACBuilderAuxiliaries
+import vc.data.TACProgramBuilder
+import vc.data.asTACExpr
+
+class PostIntervalsRewriterPatternsTest : TACBuilderAuxiliaries() {
+
+    private fun checkStat(
+        prog: TACProgramBuilder.BuiltTACProgram,
+        stat: String,
+        count: Int = 1,
+        patterns: PatternRewriter.() -> List<PatternHandler> = PatternRewriter::postIntervalsRewriterPatternList
+    ) {
+        val stats = PatternRewriter.rewriteStats(prog.code, patterns)
+        assertEquals(count, stats[stat])
+    }
+
+    /**
+     * `A / B < C`  ~~>  `A < B*C`     when B is a non-zero constant.
+     */
+    @Test
+    fun testDivLt() {
+        val prog = TACProgramBuilder {
+            d assign Div(aS, 5.asTACExpr)
+            x assign Lt(dS, cS)
+        }
+        checkStat(prog, "divLt")
+    }
+
+    /**
+     * `A / B <= C`  ~~>  `A < B*(C+1)`     when B is a non-zero constant.
+     */
+    @Test
+    fun testDivLe() {
+        val prog = TACProgramBuilder {
+            d assign Div(aS, 5.asTACExpr)
+            x assign Le(dS, cS)
+        }
+        checkStat(prog, "divLe")
+    }
+
+    /**
+     * `A / B > C`  ~~>  `A >= B*(C+1)`     when B is a non-zero constant.
+     */
+    @Test
+    fun testDivGt() {
+        val prog = TACProgramBuilder {
+            d assign Div(aS, 5.asTACExpr)
+            x assign Gt(dS, cS)
+        }
+        checkStat(prog, "divGt")
+    }
+
+    /**
+     * `A / B >= C`  ~~>  `A >= B*C`     when B is a non-zero constant.
+     */
+    @Test
+    fun testDivGe() {
+        val prog = TACProgramBuilder {
+            d assign Div(aS, 5.asTACExpr)
+            x assign Ge(dS, cS)
+        }
+        checkStat(prog, "divGe")
+    }
+
+    /**
+     * `A / B == C`  ~~>  `B*C <= A < B*(C+1)`     when B is a non-zero constant.
+     */
+    @Test
+    fun testDivEq() {
+        val prog = TACProgramBuilder {
+            d assign Div(aS, 5.asTACExpr)
+            x assign Eq(dS, cS)
+        }
+        checkStat(prog, "divEq")
+    }
+
+    /**
+     * Negative test: when the divisor is a plain variable (no `NON_ZERO_META`, not a non-zero const),
+     * the rewrite must not fire.
+     */
+    @Test
+    fun testDivLtNotRewrittenWhenDivisorMayBeZero() {
+        val prog = TACProgramBuilder {
+            d assign Div(aS, bS)
+            x assign Lt(dS, cS)
+        }
+        checkStat(prog, "divLt", count = 0)
+    }
+}
